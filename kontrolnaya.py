45c395e7ad5d30048405dcd79918983844258feb
Abstrakcionist <kondratev04g@mail.ru>
2023-02-19 21:12:00 +0500

    kontrolnaya work

--- kontrolnaya.py
+++ kontrolnaya.py
@@ -12,11 +12,23 @@ students, variants, row, place = map(int, open('input.txt').readlines())
 petya = variant(row, place, variants)
 desks = students // 2
 arr = []
+result = 0
 if (students // variants) >= 2:
-    for desk in range(1, desks + 1):
+    for desk in range(row, desks + 1):
         for lr in range(1, 3):
             if (desk != row) and (variant(desk, lr, variants) == petya):
                 arr.append([desk, lr])
     print(arr[0][0], arr[0][1])
 else:
-    print(-1)
+    result = -1
+arr = []
+if result == -1:
+    for desk in range(1, row + 1):
+        for lr in range(1, 3):
+            if (desk != row) and (variant(desk, lr, variants) == petya):
+                print(desk, lr, variant(desk, lr, variants))
+                arr.append([desk, lr])
+                result = 0
+    print(arr[-1][0], arr[-1][1])
+    if result == -1:
+        print(result)
